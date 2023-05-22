040e781480c8cdd753ebcdca115bb1738d6e1e3c
yahia <mohamedyahia207545>
2023-05-22 17:57:37 +0300

    create order

---
+++ services/orderService.js
@@ -0,0 +1,51 @@
+const asyncHandler = require('express-async-handler');
+
+const factory = require('./handlerFactory');
+const ApiError = require('../utils/apiError');
+const Product = require('../models/productModel');
+const Cart = require('../models/cartModel');
+const Order = require('../models/orderModel');
+
+// @desc   create cash order
+// @route  POST /api/v1/orders/cartId
+// @access Private/User
+exports.createCashOrder = asyncHandler(async (req, res, next) => {
+  // App setting
+  const taxPrice = 0;
+  const shippingPrice = 0;
+  // 1) Get cart depend on cartId
+  const cart = await Cart.findById(req.params.cartId);
+  if (!cart) {
+    return next(
+      new Error(`There is no such cart with id ${req.body.cartId}`, 404)
+    );
+  }
+  // 2) Get order price depend on cart price "Check if coupon apply"
+  const cartPrice = cart.totalPriceAfterDiscount
+    ? cart.totalPriceAfterDiscount
+    : cart.totalCartPrice;
+
+  const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+  // 3) Create order with default paymentMthodType cash
+  const order = await Order.create({
+    user: req.user._id,
+    cartItems: cart.cartItems,
+    shippingAddress: req.body.shippingAddress,
+    totalOrderPrice,
+  });
+  // 4) After creating order, decrement product quantity, and increment product sold
+  if (order) {
+    const bulkOption = cart.cartItems.map((item) => ({
+      updateOne: {
+        filter: { _id: item.product },
+        update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
+      },
+    }));
+    await Product.bulkWrite(bulkOption, {});
+
+    // 5) Clear cart depend on cartId
+    await Cart.findByIdAndDelete(req.params.cartId);
+  }
+
+  res.status(201).json({ status: 'success', data: order });
+});
